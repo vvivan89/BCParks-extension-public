6f6ddfc8ed421e0bb3d28beb20d0d0c40d7bf51d
Vladimir Ivanov <v.ivanov.v@gmail.com>
2021-11-02 22:08:30 -0700

    search changes

--- search.js
+++ search.js
@@ -12,7 +12,7 @@ chrome.storage.sync.get(
 			
 			//party size is here
 			const padsNeeded = Number(document.getElementById("mainContent_homeContent_ddlNights").value)
-			
+			console.log(new Date().getSeconds(), new Date().getMilliseconds())
 			//if available, proceed with booking
 			if (padsAvailable >= padsNeeded) {
 			
@@ -30,12 +30,19 @@ chrome.storage.sync.get(
 				//click on "submit" button
 				button.children[0].click()
 			}
-			//always click on search button again if the page did not start reloading
+			/*
+				always click on search button again if the page did not start reloading
+				but first, we want timer to execute exactly at xx.010 (10 milliseconds)
+				so we need to calculate the delay
+				this behavior is important to catch the sites directly when the booking date becomes available 
+				// (currently it's 7:00:00.000 PST/PDT)
+			*/
+			const ms = new Date().getMilliseconds()
+			const delay = 1010-ms
 			setTimeout(() => {
                 document.getElementById('btnSearch').click()
-                
                 //also send new message to the background to set new timeout
 				chrome.runtime.sendMessage({})
-			}, 500)
+			}, delay)
         }
     )
